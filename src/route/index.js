0dc64d6cc003357a307011876ed0eadf9f035e7f
taureau <tarasgavrilov1985@gmail.com>
2023-06-25 22:02:51 +0300

    shopcatalog xchanged

--- src/route/index.js
+++ src/route/index.js
@@ -3053,7 +3053,7 @@ router.get('/shopreview', function (req, res) {
 })
 
 // ================================================================
-outer.get('/shopcatalog', function (req, res) {
+router.get('/shopcatalog', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
